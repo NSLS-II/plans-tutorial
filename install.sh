90911cc41cd6882f6ca2aaf8c25d5afef0e6fa27
Thomas A Caswell <tcaswell@gmail.com>
2016-08-23 14:16:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 # Then open a terminal / command prompt and type or paste this:
 
 conda create --name plans-demo python=3.5 ipython matplotlib scipy
+# *nix
 source activate plans-demo
+# windows
+# activate plans-demo
 pip install boltons prettytable
 pip install http://github.com/NSLS-II/event-model/zipball/v1.1.0
 pip install http://github.com/NSLS-II/bluesky/zipball/v0.6.3
